b274ec5810ecfbaf84bd316059d94ca849dc6f7b
Suman <dobe4udie@gmail.com>
2015-07-10 02:17:47 +0530

    Generated devise views and migrated db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150709133902) do
+ActiveRecord::Schema.define(version: 20150709141256) do
 
   create_table "donations", force: :cascade do |t|
     t.string   "name"
@@ -32,6 +32,11 @@ ActiveRecord::Schema.define(version: 20150709133902) do
     t.string   "logo_uid"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "ngos_users", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "ngo_id"
   end
 
   create_table "requests", force: :cascade do |t|
