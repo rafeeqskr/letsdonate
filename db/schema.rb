123347357bde644ec43fe9798b4c1c33e0dbcf35
Suman <dobe4udie@gmail.com>
2015-07-09 19:16:29 +0530

    Migrated db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150709131213) do
+ActiveRecord::Schema.define(version: 20150709133902) do
 
   create_table "donations", force: :cascade do |t|
     t.string   "name"
@@ -32,6 +32,15 @@ ActiveRecord::Schema.define(version: 20150709131213) do
     t.string   "logo_uid"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "requests", force: :cascade do |t|
+    t.integer  "ngo_id"
+    t.integer  "user_id"
+    t.integer  "donation_id"
+    t.string   "status"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "users", force: :cascade do |t|
