8250d54ba6393f6ac4e7b215763c1895d0012cd8
Vivek Sampara <ravensnowbird@gmail.com>
2015-07-09 18:45:34 +0530

    added donation model controller and views

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150709125458) do
+ActiveRecord::Schema.define(version: 20150709131115) do
+
+  create_table "donations", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.string   "category"
+    t.datetime "expiry"
+    t.string   "city"
+    t.integer  "user_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
